6d42c03a63827c51b65a401c29d85b5588c63430
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-06 08:03:17 -0300

    play with loops

---
+++ basics/target/debug/deps/loops-60c17ccd240d7d9c.d
@@ -0,0 +1,5 @@
+/home/elpoeta/workspaces/rust/play-rust/basics/target/debug/deps/loops-60c17ccd240d7d9c.rmeta: src/bin/loops.rs
+
+/home/elpoeta/workspaces/rust/play-rust/basics/target/debug/deps/loops-60c17ccd240d7d9c.d: src/bin/loops.rs
+
+src/bin/loops.rs:
