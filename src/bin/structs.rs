bb07a489a21fa4f8344426c97f757b1d257d20f8
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-09 08:16:09 -0300

    stuct as tuple

--- src/bin/structs.rs
+++ src/bin/structs.rs
@@ -4,6 +4,8 @@ struct RGBColor {
     blue: u8,
 }
 
+struct ColorTuple(u8, u8, u8);
+
 fn main() {
     let mut rgb = RGBColor {
         red: 255,
@@ -14,4 +16,8 @@ fn main() {
     rgb.red = 4;
 
     println!("R: {}, G: {}, B: {}", rgb.red, rgb.green, rgb.blue);
+
+    let tuple_color = ColorTuple(67, 21, 10);
+
+    println!("{}", tuple_color.0);
 }
